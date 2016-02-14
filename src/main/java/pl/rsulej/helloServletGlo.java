c895b0e891032f1c3954e0f5046480c66b46968e
Nuta <rasulej@gazeta.pl>
2016-02-14 15:07:27 +0100

    abc

---
+++ src/main/java/pl/rsulej/helloServletGlo.java
@@ -0,0 +1,17 @@
+package pl.rsulej;
+
+        import javax.servlet.GenericServlet;
+        import javax.servlet.ServletException;
+        import javax.servlet.ServletRequest;
+        import javax.servlet.ServletResponse;
+        import javax.servlet.annotation.WebServlet;
+        import java.io.IOException;
+
+@WebServlet(value="/", name="helloServletGlo")
+public class helloServletGlo extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("helloServletGlo RS!!<a href='./hello'>hello<");
+        res.getWriter().println("2. helloServletGlo RS!!");
+    }
+}
